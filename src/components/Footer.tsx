21ac8fec99de178237081e2e0795350ccb3e03a6
MarkiMark01 <markishgroup@gmail.com>
2025-02-09 16:36:19 +0100

    created Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+import styles from "../styles/footer.module.scss";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <span className={styles.footer__text}>2024 &copy; Markiyan Marych</span>
+    </footer>
+  );
+};
+export default Footer;
