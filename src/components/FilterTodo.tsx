ac2eb8d9f1b67bab8894a2705ba4ab9f604795c5
MarkiMark01 <markishgroup@gmail.com>
2025-02-09 13:43:21 +0100

    created FilterTodo

---
+++ src/components/FilterTodo.tsx
@@ -0,0 +1,11 @@
+
+
+const FilterTodo = ({ filter, setFilter }: { filter: string; setFilter: (value: string) => void }) => (
+    <input
+      type="text"
+      placeholder="Filter tasks"
+      value={filter}
+      onChange={(e) => setFilter(e.target.value)}
+    />
+  );
+export default FilterTodo;
